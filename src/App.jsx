ecb4c0cc7bd0411b98319d3c4abd3cde0cb3a7dc
shahrishav <shahrishav11@gmail.com>
2026-04-08 14:40:45 +0545

    Initial commit - memory card game

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Card } from "./components/Card";
+import { GameHeader } from "./components/GameHeader";
+import { WinMessage } from "./components/WinMessage";
+import { useGameLogic } from "./hooks/useGameLogic";
+
+const cardValues = [
+  "🐶",
+  "🦊",
+  "🐼",
+  "🐹",
+  "🐨",
+  "🐯",
+  "🐷",
+  "🐥",
+  "🐶",
+  "🦊",
+  "🐼",
+  "🐹",
+  "🐨",
+  "🐯",
+  "🐷",
+  "🐥",
+];
+
+
+
+function App() {
+  const {
+    cards,
+    score,
+    moves,
+    handleCardClick,
+    initializeGame,
+    isGameComplete,
+  } = useGameLogic(cardValues);
+
+  return (
+    <div className="app">
+      <GameHeader score={score} moves={moves} onReset={initializeGame} />
+
+      {isGameComplete && <WinMessage moves={moves} />}
+
+      <div className="cards-grid">
+        {cards.map((card) => (
+          <Card card={card} onClick={handleCardClick} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
